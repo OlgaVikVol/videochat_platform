0df1ea2bed513a1dfaebc71191961d00a256e9d1
Olga Volobueva <olvik1605@gmail.com>
2024-04-23 22:02:13 -0400

    add isScheduleMeeting MeetingModal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,20 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import '@stream-io/video-react-sdk/dist/css/styles.css';
+import 'react-datepicker/dist/react-datepicker.css'
 
 import "./globals.css";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
