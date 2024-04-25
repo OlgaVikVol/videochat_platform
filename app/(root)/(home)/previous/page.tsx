c795c75b03ee1be8403948b68dd1c106152aa4cc
Olga Volobueva <olvik1605@gmail.com>
2024-04-24 22:14:04 -0400

    added CallList, MeetingCard, useGetCalls

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import CallList from '@/components/CallList';
 
 const Previous = () => {
 	return (
 		<section className='flex size-full flex-col gap-5 text-white'>
 			<h1 className='text-3xl font-bold'>
-				Previous
+				Previous Calls
 			</h1>
+
+			<CallList type='ended' />
 		</section>
 	)
 }
--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import CallList from '@/components/CallList';
 
 const Upcoming = () => {
 	return (
 		<section className='flex size-full flex-col gap-5 text-white'>
 			<h1 className='text-3xl font-bold'>
-				Upcoming
+				Upcoming Meeting
 			</h1>
+			<CallList type='upcoming' />
 		</section>
 	)
 }
